b727a0d4a79ef585535bd16b92dd3006d7397804
Perryno <andreacaforio74@gmail.com>
2023-03-29 18:00:38 +0200

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import MyNav from './components/MyNav';
+import MyFooter from './components/MyFooter';
+import Welcome from './components/Welcome';
+import AllBooks from './components/AllBooks';
+
+function App() {
+  return (
+    <>
+      <MyNav /> 
+    <Container>
+
+    <Welcome/>
+
+      <MyFooter bottone="info"/>
+
+
+    </Container>
+
+    <AllBooks/>
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/components/AllBooks.jsx
@@ -0,0 +1,22 @@
+import Books from '../data/fantasy.json';
+
+import Card from 'react-bootstrap/Card';
+
+function AllBooks() {
+    return (
+        {Books.map((book,index) => (
+            <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={book.img} />
+      <Card.Body>
+        <Card.Title>{book.title}</Card.Title>
+        <Card.Text>
+        {book.price}
+        </Card.Text>
+       
+      </Card.Body>
+    </Card>
+        ))}
+    )
+}
+
+export default AllBooks

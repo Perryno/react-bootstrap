ecf4f9bf04c3ad73466cebdf1875c7cb522c8281
Perryno <andreacaforio74@gmail.com>
2023-03-30 09:12:05 +0200

    format

--- src/components/AllBooks.jsx
+++ src/components/AllBooks.jsx
@@ -1,22 +1,21 @@
-import Books from '../data/fantasy.json';
+import Books from "../data/fantasy.json";
 
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 
 function AllBooks() {
-    return (
-        {Books.map((book,index) => (
-            <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={book.img} />
-      <Card.Body>
-        <Card.Title>{book.title}</Card.Title>
-        <Card.Text>
-        {book.price}
-        </Card.Text>
-       
-      </Card.Body>
-    </Card>
-        ))}
-    )
+  return (
+    <>
+      {Books.map((book, index) => (
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={book.img} />
+          <Card.Body>
+            <Card.Title>{book.title}</Card.Title>
+            <Card.Text>{book.price}</Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
+    </>
+  );
 }
 
-export default AllBooks
+export default AllBooks;
